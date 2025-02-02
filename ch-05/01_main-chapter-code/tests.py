397748f5aa8a84dd142d6c83549228b6b153dc53
tchtinku <tchtinku@gmail.com>
2025-02-03 00:14:28 +0530

    previous_chapters.py 20250203

--- ch-05/01_main-chapter-code/tests.py
+++ ch-05/01_main-chapter-code/tests.py
@@ -25,4 +25,64 @@ def other_settings():
         "weight_decay": 0.1
     }
     
-def test_main(gpt_config,)
+def test_main(gpt_config, other_settings):
+    train_losses, val_losses, tokens_seen, model = main(gpt_config, other_settings)
+    
+    assert len(train_losses) == 39, "Unexpected number of training losses"
+    assert len(val_losses) == 39, "Unexpected number of validation losses"
+    assert len(tokens_seen) == 39, "Unexpected number of tokens seen"
+    
+def check_file_size(url, expected_size):
+    parsed_url = urlparser(url)
+    if parsed_url.scheme == "https":
+        conn = http.client.HTTPSConnection(parsed_url.netloc)
+    else:
+        conn = http.client.HTTPConnection(parsed_url.netloc)
+        
+    conn.request("HEAD", parsed_url.path)
+    response = conn.getresponse()
+    if response.status != 200:
+        return False, f"{url} not accessible"
+    size = int(size)
+    if size != expected_size:
+        return False, f"{url} file has expected size {expected_size}, but got {size}"
+    return True, f"{url} file size is correct"
+
+def test_model_files():
+    def check_model_files(base_url):
+        
+        model_size = "124M"
+        files = {
+            "checkpoint": 77,
+            "encoder.json": 1042301,
+            "hparams.json": 90,
+            "model.ckpt.data-00000-of-00001": 497759232,
+            "model.ckpt.index": 5215,
+            "model.ckpt.meta": 471155,
+            "vocab.bpe": 456318
+        }
+        
+        for file_name, expected_size in files.items():
+            url = f"{base_url}/{model_size}/{file_name}"
+            valid, message = check_file_size(url, expected_size)
+            assert valid, message
+            
+        model_size = "355M"
+        files = {
+            "checkpoint": 77,
+            "encoder.json": 1042301,
+            "hparams.json": 91,
+            "model.ckpt.data-00000-of-00001": 1419292672,
+            "model.ckpt.index": 10399,
+            "model.ckpt.meta": 926519,
+            "vocab.bpe": 456318
+        }
+        
+        for file_name, expected_size in files.items():
+            url = f"{base_url}/{model_size}/{file_name}"
+            valid, message = check_file_size(url, expected_size)
+            assert valid, message
+            
+    check_model_files(base_url="https://openaipublic.blob.core.windows.net/gpt-2/models")
+    check_model_files(base_url="https://f001.backblazeb2.com/file/LLMs-from-scratch/gpt2")
+    
